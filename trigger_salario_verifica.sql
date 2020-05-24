62adf046346ea1ca34c7cb865773d3c3c1a5003a
fuscati <up201806140@fe.up.pt>
2020-05-24 16:30:36 +0100

    message before and after trigger

--- trigger_salario_verifica.sql
+++ trigger_salario_verifica.sql
@@ -1,3 +1,4 @@
+select CC as Fucionario_viciado_CC, nome from Funcionario_Viciado;
 insert into Aposta values (40,'2019-08-01',10000,'L',0.7);
 insert into AppliedApostaCliente values ('12345420',40);
 select CC as Fucionario_viciado_CC, nome from Funcionario_Viciado;
