56a07e7d76b18fa3a14743ed6d7d1fa8ef6802b3
Me-Salva <65774295+Me-Salva@users.noreply.github.com>
2020-05-25 16:15:44 +0100

    Update interrogaçao-mesa-mais-lucro.sql

--- interrogaçao-mesa-mais-lucro.sql
+++ interrogaçao-mesa-mais-lucro.sql
@@ -1,14 +1,14 @@
 .mode columns
 .headers on
 .nullvalue	NULL
-DROP VIEW valorGanho IF EXISTS
+DROP VIEW IF EXISTS valorGanho;
 CREATE VIEW valorGanho
 AS
-  SELECT*,sum(valor) AS Ganho FROM Mesa,AppliedApostaMesa,Aposta WHERE Mesa.idMesa=AppliedApostaMesa.idMesa AND Aposta.idAposta=AppliedApostaMesa.idAposta AND resultado='W'
+  SELECT*,sum(valor) AS Ganho FROM Mesa,AppliedApostaMesa,Aposta WHERE Mesa.idMesa=AppliedApostaMesa.idMesa AND Aposta.idAposta=AppliedApostaMesa.idAposta AND resultado='W';
 
-DROP VIEW valorPerdido IF EXISTS
+DROP VIEW IF EXISTS valorPerdido;
 CREATE VIEW valorPerdido
 AS
-  SELECT*,sum(valor) AS Perdido FROM Mesa,AppliedApostaMesa,Aposta WHERE Mesa.idMesa=AppliedApostaMesa.idMesa AND Aposta.idAposta=AppliedApostaMesa.idAposta AND resultado='L'
+  SELECT*,sum(valor) AS Perdido FROM Mesa,AppliedApostaMesa,Aposta WHERE Mesa.idMesa=AppliedApostaMesa.idMesa AND Aposta.idAposta=AppliedApostaMesa.idAposta AND resultado='L';
 
-SELECT idMesa,max(Ganho-Perdido) AS lucro FROM (valorGanho natural join valorPerdido); valor
+SELECT idMesa,max(Ganho-Perdido) AS lucro FROM (valorGanho natural join valorPerdido);
