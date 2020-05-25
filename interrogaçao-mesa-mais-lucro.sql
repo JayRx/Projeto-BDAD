e383f9accb06374aff98c49cf0d4c29ca9a15bd5
Me-Salva <65774295+Me-Salva@users.noreply.github.com>
2020-05-25 15:50:46 +0100

    Update interrogaçao-mesa-mais-lucro.sql

--- interrogaçao-mesa-mais-lucro.sql
+++ interrogaçao-mesa-mais-lucro.sql
@@ -4,11 +4,11 @@
 DROP VIEW valorGanho IF EXISTS
 CREATE VIEW valorGanho
 AS
-  SELECT*FROM sum(valor),Mesa,AppliedApostaMesa,Aposta AS Ganho WHERE Mesa.idMesa=AppliedApostaMesa.idMesa AND Aposta.idAposta=AppliedApostaMesa.idAposta AND resultado='W'
+  SELECT*,sum(valor) AS Ganho FROM Mesa,AppliedApostaMesa,Aposta WHERE Mesa.idMesa=AppliedApostaMesa.idMesa AND Aposta.idAposta=AppliedApostaMesa.idAposta AND resultado='W'
 
 DROP VIEW valorPerdido IF EXISTS
 CREATE VIEW valorPerdido
 AS
-  SELECT*FROM sum(valor),Mesa,AppliedApostaMesa,Aposta AS Perdido WHERE Mesa.idMesa=AppliedApostaMesa.idMesa AND Aposta.idAposta=AppliedApostaMesa.idAposta AND resultado='L'
+  SELECT*,sum(valor) AS Perdido FROM Mesa,AppliedApostaMesa,Aposta WHERE Mesa.idMesa=AppliedApostaMesa.idMesa AND Aposta.idAposta=AppliedApostaMesa.idAposta AND resultado='L'
 
 SELECT idMesa,max(Ganho-Perdido) AS lucro FROM (valorGanho natural join valorPerdido); valor
